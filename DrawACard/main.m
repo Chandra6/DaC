df9d55c73fd925a3a2b18c0903726bdd8c588cf0
Chandra Polepeddi <cpolepeddi@yahoo.com>
2014-07-21 20:03:37 -0400

    Initial Commit

---
+++ DrawACard/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DrawACard
+//
+//  Created by iCS on 21/07/14.
+//  Copyright (c) 2014 MyOrg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
